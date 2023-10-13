dcfbabdcabfec6bbeb5b008a176c9e417ad34da6
Daniel Washington Ignacio <danielvertigo@hotmail.com>
2023-10-13 13:58:15 -0300

    Initial Commit

---
+++ ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Daniel Washington Ignacio on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
